cc32944d8d762b896321152e106918f28d28e8e3
reese <120700018+rosematcha@users.noreply.github.com>
2025-06-10 13:25:58 -0500

    quotes update

--- src/assets/quotes.js
+++ src/assets/quotes.js
@@ -6,15 +6,13 @@ const quotes = [
   "oh god, a waking nightmare! i live a life so hollow that i’m not even there",
   "soo hungry need to find my wife and head to pf changs",
   "i'm using tilt controls!",
-  "most may think chiikawa is a hamster, but chiikawa is actually just a 'cute little thing'",
-  "an idiot plot is one which is 'kept in motion solely by virtue of the fact that everybody involved is an idiot'",
   "there are cathedrals everywhere for those with the eyes to see",
-  "you're like a baby salamander dressed in a suit",
   "in another life, I would have really liked just doing laundry and taxes with you",
   "he was in the amazon with my mom when she was researching spiders just before she died",
   "sparkle on! it's wednesday! don't forget to be yourself!",
   "i miss my wife, tails. i miss her a lot. i'll be back.",
   "life is great, man! you can eat at denny's, wear a hat, whatever you want!",
+  "it's always the wrong thing when it's me who's saying it",
   "once you've seen the signs around the barn, it becomes impossible to see the barn"
 ];
 
